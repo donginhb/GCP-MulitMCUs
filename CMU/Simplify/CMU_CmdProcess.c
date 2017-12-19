1ae7ed127cbb6aad8dc3280ca3f569dc8a06885a
陈晓聪 <a1194457876@outlook.com>
2017-12-19 18:15:46 +0800

    完善Bootloader接口

--- CMU/Simplify/CMU_CmdProcess.c
+++ CMU/Simplify/CMU_CmdProcess.c
@@ -18,17 +18,18 @@
 #include "AbstractionLayer.h"
 #include "CMU_CmdProcess.h"
 #include "CMU_DataStructDef.h"
-//#include "CMU_ExApi.h"
 #include "CMU_ErrorMana.h"
 
 #include "DataType/DataType.h"
-
+#include "Version/Version.h"
 
 /*****************************************************************************
  * 私有成员定义及实现
  ****************************************************************************/
-
-static COM_DATA_PACK   m_sPackData;     //数据包缓冲区
+#define CMU_SNED_BUFF_LEN       (128)
+static uBit8 m_uSendBuff[CMU_SNED_BUFF_LEN] = {0};
+static COM_DATA_PACK   m_sRecvPackData = {0};     //接收数据缓冲区
+static COM_DATA_PACK   m_sSendPackData = {{0}, 0, m_uSendBuff};     //发送数据缓冲区
 
 
 /**
@@ -48,6 +49,23 @@ uBit32 SYS_UpdateAPP(uBit8 *pBuff, uBit32 ulSize);
   */
 uBit32 SYS_UpdateSubAPP(uBit8 *pBuff, uBit32 ulSize);
 
+
+/**
+  * @brief  延时复位
+  * @param  ulDelayMs 延时的时间(单位:MS)
+  * @retval None
+  */
+void CoreCtrl_ResetSystemDelay(uBit32 ulDelayMs);
+
+
+/**
+  * @brief  系统逻辑控制版本获取
+  * @param  pVersion 版本结构指针
+  * @retval 0-成功 非0-失败
+  */
+uBit32 SYS_GetSLCVersion(SOFTWARE_VERSION* pVersion);
+
+
 /*****************************************************************************
  * 内部指令实现接口
  ****************************************************************************/
@@ -60,27 +78,40 @@ uBit32 SYS_UpdateSubAPP(uBit8 *pBuff, uBit32 ulSize);
 static uBit32 Sys_SetNomalCmdProcess(COM_DATA_PACK *pRcvCtrlData)
 {
     uBit32 ulRet = CMU_ERR_INVALID_CMD;
-    //CMU_EXTERNAL_FUN_TEBLE *pExternFun = CMU_GetExternFunAddr();
     
     switch(pRcvCtrlData->ulID.ulComDataID.ulCmdIndex)
     {
     case SYS_SETCMD_UPDATE_SLC:                        //SLC升级数据
-        //ulRet = pExternFun->pf_SYS_UpdateSLC((Bit8*)pRcvCtrlData->pDataBuf, 
-        //    pRcvCtrlData->ulDataLen);
         ulRet = SYS_UpdateSubAPP((uBit8*)pRcvCtrlData->pDataBuf, pRcvCtrlData->ulDataLen);
+        
+        if (ulRet == CMU_ERR_SUCCESS)
+        {
+            ulRet = CMU_ERR_NO_ACK;
+        }
+        
         break;
     case SYS_SETCMD_UPDATE_IPO:                        //IPO升级数据
-        //ulRet = pExternFun->pf_SYS_UpdateIPO((Bit8*)pRcvCtrlData->pDataBuf, 
-        //    pRcvCtrlData->ulDataLen);
-        
         ulRet = SYS_UpdateAPP((uBit8*)pRcvCtrlData->pDataBuf, pRcvCtrlData->ulDataLen);
+        
+        if (ulRet == CMU_ERR_SUCCESS)
+        {
+            ulRet = CMU_ERR_NO_ACK;
+        }
+        
         break;
     case SYS_SETCMD_SYS_RESET:
-        //pExternFun->pf_SYS_Reset();
-        ulRet = CMU_ERR_SUCCESS;
-        break;
+        {
+            //设置回发数据
+            m_sSendPackData.ulID.ulFrameID = pRcvCtrlData->ulID.ulFrameID;
+            m_sSendPackData.ulDataLen = 0;
+            
+            //执行指令
+            CoreCtrl_ResetSystemDelay(500);
+            ulRet = CMU_ERR_SUCCESS;
+            break;
+        }
     case SYS_SETCMD_SET_SLC_FLAG:
-        //pExternFun->pf_SYS_WriteSLCProgID();
+        
         ulRet = CMU_ERR_SUCCESS;
         break;
         
@@ -99,6 +130,22 @@ static uBit32 Sys_SetNomalCmdProcess(COM_DATA_PACK *pRcvCtrlData)
 static uBit32 Sys_GetNomalCmdProcess(COM_DATA_PACK *pRcvCtrlData)
 {
     uBit32 ulRet = CMU_ERR_INVALID_CMD;
+    
+    switch(pRcvCtrlData->ulID.ulComDataID.ulCmdIndex)
+    {
+    case SYS_GETCMD_SLC_VER:                        //获取系统版本
+        {
+            //设置回发数据
+            m_sSendPackData.ulID.ulFrameID = pRcvCtrlData->ulID.ulFrameID;
+            m_sSendPackData.ulDataLen = sizeof(SOFTWARE_VERSION);
+            m_sSendPackData.ulID.ulComDataID.ulTransmitFrame = TRANSMIT_SELF_FRAME;
+            
+            ulRet = SYS_GetSLCVersion((SOFTWARE_VERSION *)m_sSendPackData.pDataBuf);
+            
+            break;
+        }
+    default:break;
+    }
     
     return ulRet;
 }
@@ -120,7 +167,7 @@ uBit32 CMU_CmdProcess(COM_DATA_PACK *pRcvCtrlData)
     switch (pRcvCtrlData->ulID.ulComDataID.ulDataType)
     {
     case DATA_TYPE_SYS:
-        if (pRcvCtrlData->ulID.ulComDataID.ulCmdType == DATA_OPRE_TYPE_SET) //设置指令
+        if (pRcvCtrlData->ulID.ulComDataID.ulOperType == DATA_OPRE_TYPE_SET) //设置指令
         {
             ulRet = Sys_SetNomalCmdProcess(pRcvCtrlData);
         }
@@ -190,17 +237,21 @@ uBit32 COM_PL_ResetRcvCtrlData(uBit32 ulRcvId, uBit8 *pRcvBuf, uBit32 ulLen)
 uBit32 CMU_MainRcvProc(void)
 {
     //查询数据包接收情况
-    uBit32 ulRet = COM_AL_GetPack(&m_sPackData);
+    uBit32 ulRet = COM_AL_GetPack(&m_sRecvPackData);
     
     //假如接收到数据,则进行数据解析
     if (ulRet==CMU_ERR_SUCCESS) //成功收到数据包
     {
-        ulRet = CMU_CmdProcess(&m_sPackData);
+        ulRet = CMU_CmdProcess(&m_sRecvPackData);
         
-        //接收到数据包，但处理数据包失败，则直接发送错误回包
-        if ((ulRet != CMU_ERR_SUCCESS) && (ulRet != CMU_ERR_OUTMODED) && (ulRet != CMU_ERR_NO_ACK))
+        if (ulRet == CMU_ERR_SUCCESS)
         {
-            ulRet = CMU_SendResponsePack(m_sPackData.ulID.ulFrameID, ulRet);
+            COM_AL_SendPack(m_sSendPackData.ulID.ulFrameID, m_sSendPackData.pDataBuf, m_sSendPackData.ulDataLen);
+        }
+        //接收到数据包，但处理数据包失败，则直接发送错误回包
+        else if (ulRet != CMU_ERR_NO_ACK)
+        {
+            ulRet = CMU_SendResponsePack(m_sRecvPackData.ulID.ulFrameID, ulRet);
         }
     }
     
